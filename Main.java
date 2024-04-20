64c3b9fd3a9ef96efc683121971e009728b25686
Salim Proctor <98918813+salimproctor@users.noreply.github.com>
2024-04-20 07:38:59 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,24 +1,31 @@
 //Main
-import Model.*;
-import Controller.*;
-import View.*;
+
+import controller.CarController;
+import model.CarModel;
+import view.CarView;
+import view.ViewFormVoiture;
+
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-           // public void run() {
-                Model model = new Model();
-                View view = new View();
-                Controller controller = new Controller(model, view);
-                view.setController(controller);
-                view.showHomePage(); // Afficher la page d'accueil
+            // public void run() {
+//            Model model = new Model();
+//            View view = new View();
+//            Controller controller = new Controller(model, view);
+//            view.setController(controller);
+//            // view.showHomePage(); // Afficher la page d'accueil
 
 
-                ViewFormVoiture formulaireVoiture = new ViewFormVoiture();
-                ControllerVoiture controllerVoiture = new ControllerVoiture(formulaireVoiture);
-                formulaireVoiture.afficher();
-           // }
+            // Création du modèle, de la vue et du contrôleur
+
+            ViewFormVoiture viewFormVoiture = new ViewFormVoiture();
+            CarModel carModel = new CarModel();
+            CarView carView = new CarView();
+            CarController carController = new CarController(carModel, carView, viewFormVoiture);
+
+            carController.initialize();
         });
     }
 }

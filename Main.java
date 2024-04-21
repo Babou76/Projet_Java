86bc39ad39f714c0d93b84bd6c7d1058c9d8bf9d
leo cousin <117753731+LEODOZO@users.noreply.github.com>
2024-04-21 21:11:58 +0200

    Update Main.java
    test de nv composant ?

--- Main.java
+++ Main.java
@@ -5,8 +5,12 @@ import controller.CarController;
 import model.CarModel;
 import view.CarView;
 import view.ViewFormVoiture;
-
+import java.util.HashMap;
+import java.util.Map;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public static void main(String[] args) {
     SwingUtilities.invokeLater(new Runnable() {
@@ -25,6 +29,7 @@ public static void main(String[] args) {
             CarController carController = new CarController(carModel, carView, viewFormVoiture);
             carController.setViewFormVoiture(viewFormVoiture); // Assignez l'instance de ViewFormVoiture à votre CarController
             carController.initialize();
+            SwingUtilities.invokeLater(CarCarousel::new);
             }
         });
     }
